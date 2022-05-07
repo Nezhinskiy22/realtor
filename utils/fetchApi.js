9d410f1ea6b86f71b9eb37a5ef7d36e69324932d
Nezhinskiy22 <Nezhinskiy22@gmail.com>
2022-05-07 20:00:08 +0300

    fetching added

---
+++ utils/fetchApi.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const baseUrl = "https://bayut.p.rapidapi.com";
+
+export const fetchApi = async (url) => {
+  const { data } = await axios.get(url, {
+    headers: {
+      "X-RapidAPI-Host": "bayut.p.rapidapi.com",
+      "X-RapidAPI-Key": "568f93900amsh70cb898ae51211ap19e818jsn36e1d7d408d7",
+    },
+  });
+};
